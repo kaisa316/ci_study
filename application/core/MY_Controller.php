239e5fa10fe2152e09d525ea250e6a19f9916648
kaisa316 <490788241@qq.com>
2019-07-03 15:23:57 +0800

    发送到本地sentry 服务器

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -3,6 +3,7 @@
 class MY_Controller extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
-		Sentry\init(['dsn' => 'https://c548e5b8a4e24806b12b554f4a199ea7@sentry.io/1370870' ]);
+		//Sentry\init(['dsn' => 'https://c548e5b8a4e24806b12b554f4a199ea7@sentry.io/1370870' ]);
+		Sentry\init(['dsn' => 'http://1576aac9570646ea88025552a603175c@127.0.0.1:9000/2' ]);//发送到本地sentry 服务
 	}		
 }
